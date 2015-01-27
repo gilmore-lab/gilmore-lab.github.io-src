62c8eeb07f7df108990131dc5654ac1963dafcbf
rogilmore <thatrickgilmore@gmail.com>
2015-01-27 11:41:51 -0500

    document update_github.sh

--- update_github.sh
+++ update_github.sh
@@ -1,11 +1,19 @@
+# update_github.sh
+# 	Copies files from Pelican-generated source directory to directory where GitHub
+#	website files belong.
+#
+# rogilmore (rick.o.gilmore@gmail.com)
+# 2015-01-27
+
 START=$(pwd)
-echo ">>> Making backup.\n"
+echo ">>> Making backup."
+rm -rf ~/Sites/gilmore-lab.github.io-backup
 cp -r ~/Sites/gilmore-lab.github.io ~/Sites/gilmore-lab.github.io-backup
 cd ~/Sites/gilmore-lab.github.io
 git checkout master
 echo ">>> Copying files from src to destination."
 cp -r ~/Sites/gilmore-lab.github.io-src/output/ .
-git add *.html author/ pages/ tag/ theme/
+git add *.html author/ pages/ tag/ theme/ images/
 echo "Committing changes."
 git commit -m "Updating site."
 echo ">>> Pushing changes to remote."
